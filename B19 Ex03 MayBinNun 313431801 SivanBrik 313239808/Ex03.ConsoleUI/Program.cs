35002a40dde2644f0c1eefda70407ef1383dd394
maybinnun@gmail.com <maybinnun@gmail.com>
2019-09-01 13:30:18 +0300

    ConsoleUi

---
+++ B19 Ex03 MayBinNun 313431801 SivanBrik 313239808/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex03.GarageLogic;
+using static Ex03.GarageLogic.FuelEngine;
+using static Ex03.GarageLogic.Motorcycle;
+
+namespace Ex03.ConsoleUI
+{
+
+    // $G$ CSS-999 (-4) The Class must have an access modifier.
+   
+    class Program
+    {
+        // $G$ CSS-999 (-4) The method must have an access modifier.
+        static void Main(string[] args)
+        {
+            GarageUI garage = new GarageUI();
+            garage.GargeOpen();
+        }
+    }
+}
---
+++ Ex03.ConsoleUI/UIValidation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Ex03.GarageLogic.FuelEngine;
+
+namespace Ex03.ConsoleUI
+{
+    class UIValidation
+    {
+
+        public static eFuel FuelTypeValidation(string i_FuelType)
+        {
+            eFuel fuelType;
+            bool isValid = Enum.TryParse(i_FuelType , out fuelType);
+
+            if (!isValid)
+            {
+                throw new FormatException("This type of fuel does not exist in the system");
+            }
+
+            return fuelType;
+        }
+
+        public static bool IsANumberValidation(string i_LicensNumber)
+        {
+            long numOfLicens = 0;
+            bool isValid = long.TryParse(i_LicensNumber, out numOfLicens);
+
+            if (!isValid)
+            {
+                throw new FormatException("Please use only numbers:");
+            }
+
+            return isValid;
+        }
+
+        public static int EnergyAmount(string i_EnergyAmount)
+        {
+            int energyAmount = 0;
+            bool isValid = int.TryParse(i_EnergyAmount, out energyAmount);
+
+            if (!isValid)
+            {
+                throw new FormatException("Please use only numbers:");
+            }
+
+            return energyAmount;
+        }
+
+        public static string UserStringValidation(string i_UserName)
+        {
+            bool isValid = true;
+
+            for (int i = 0; i < i_UserName.Length; i++)
+            {
+                if (!Char.IsLetter(i_UserName[i]))
+                {
+                    isValid = false;
+                }
+            }
+
+            if (!isValid)
+            {
+                throw new FormatException("Please use only letters:");
+            }
+
+            return i_UserName;
+        }
+
+    }
+}
